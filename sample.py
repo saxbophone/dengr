1a08dd70fb7c81c34668048b8d70ae71f95e06d4
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2017-06-27 00:47:35 +0100

    Adapted another algorithm for plotting equidistant points Does sort of what I want

--- sample.py
+++ sample.py
@@ -35,5 +35,36 @@ def draw_spiral(_a, _b, _step=0.5, loops=5):
     turtle.up()
     turtle.done()
 
+
+def spiral_points(inner_radius=25, point_distance=1, pitch=0.0015):
+    """generate points on an Archimedes' spiral
+    with `arc` giving the length of arc between two points
+    and `separation` giving the distance between consecutive 
+    turnings
+    - approximate arc length with circle arc at given distance
+    - use a spiral equation r = b * phi
+    """
+    def p2c(r, phi):
+        """polar to cartesian
+        """
+        return (r * math.cos(phi), r * math.sin(phi))
+
+    turtle.color("blue")
+    turtle.down()
+    turtle.goto(0, 0)
+
+    # initialize the next point in the required distance
+    r = point_distance
+    b = pitch / (2 * math.pi)
+    # find the first phi to satisfy distance of `arc` to the second point
+    phi = float(r) / b
+    while True:
+        turtle.goto(p2c(r, phi))
+        # advance the variables
+        # calculate phi that will give desired arc length at current radius
+        # (approximating with circle)
+        phi += float(point_distance) / r
+        r = inner_radius + b * phi
+
 # e.g.
 # draw_spiral(25, 0.0015, 15, 10000)
