60d833b4bd97829432e5bf12ce9553d0d4872601
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2017-07-02 23:43:10 +0100

    Change demo program to read parameters from the command-line My 'standard' parameters so far are: 1500 25 58 359849

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <assert.h>
 #include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "dengr.h"
 
 
-int main(void) {
+int main(int argc, char* argv[]) {
+    // make sure we have enough arguments (we need 5)
+    if(argc < 6) {
+        fprintf(stderr, "Not enough arguments given! (Need 5)\n");
+        return 1;
+    }
+    int64_t track_pitch, inner_radius, outer_radius;
+    size_t sector_count;
+    // retrieve arguments
+    if(sscanf(argv[1], "%" SCNd64, &track_pitch) != 1) {
+        return 1;
+    }
+    if(sscanf(argv[2], "%" SCNd64, &inner_radius) != 1) {
+        return 1;
+    }
+    if(sscanf(argv[3], "%" SCNd64, &outer_radius) != 1) {
+        return 1;
+    }
+    if(sscanf(argv[4], "%zu", &sector_count) != 1) {
+        return 1;
+    }
+    char* output_file_name = argv[5];
     // a 'chequered' image
     dengr_pixel_t image_pixels[2][2] = {
         { DENGR_PIXEL_BLACK, DENGR_PIXEL_WHITE, },
@@ -26,17 +48,17 @@ int main(void) {
     }
     // create CD format specifications
     dengr_cd_brief_spec_t cd_brief = {
-        .track_pitch = 1500,
-        .inner_radius = dengr_mm_to_nm(25),
-        .outer_radius = dengr_mm_to_nm(58),
-        .sector_count = 359849, // number of sectors on my CD-RW discs
+        .track_pitch = track_pitch,
+        .inner_radius = dengr_mm_to_nm(inner_radius),
+        .outer_radius = dengr_mm_to_nm(outer_radius),
+        .sector_count = sector_count, // number of sectors on my CD-RW discs
     };
     // get full specs of CD format
     dengr_cd_full_spec_t cd_specs = dengr_brief_spec_to_full_spec(cd_brief);
     printf("CD accessible data in bytes: %zu\n", cd_specs.capacity);
     printf("CD track length in nm: %" PRId64 "\n", cd_specs.track_length);
     // open file for writing output to
-    FILE* file_handle = fopen("output.pcm", "wb");
+    FILE* file_handle = fopen(output_file_name, "wb");
     // guard against error opening file
     assert(file_handle != NULL);
     // convert image to audio data and save to file
