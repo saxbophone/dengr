7ced82d7f6c9f982245e9e73be0cdc3be117c8e9
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-01 00:51:00 +0100

    Add Pit/Land data type

---
+++ dengr/Pit.hpp
@@ -0,0 +1,20 @@
+#ifndef COM_SAXBOPHONE_DENGR_PIT
+#define COM_SAXBOPHONE_DENGR_PIT
+
+// TODO: Change to namespace com::saxbophone::dengr when move to C++20 complete
+namespace dengr {
+    /**
+     * @brief A single Pit or Land on the physical reflective layer of the disc
+     * @remarks This represents the NRZI-encoded version of the sequence of
+     * Channel Bits formed by the data in a Channel Frame.
+     * @details Definition of the pits/lands can be found in ECMA-130, sec. 11.1
+     * @details Definition of how Channel Bits are encoded into these can be
+     * found in ECMA-130, sec. 19.4
+     */
+    enum Pit : bool {
+        PIT = true,   /**< A depression on the track's surface */
+        LAND = false, /**< A flat area on the track's surface */
+    };
+}
+
+#endif // include guard
