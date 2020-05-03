8812fe793db7576b740bd17860c5b6fcd5814390
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-03 11:31:03 +0100

    Document scramble/unscramble functions

--- dengr/scrambling.hpp
+++ dengr/scrambling.hpp
@@ -14,8 +14,18 @@
  * @details This follows the specification in Annex B of ECMA-130.
  */
 namespace com::saxbophone::dengr::scrambling {
+    /**
+     * @brief Scrambles the given Mode2 Raw Sector
+     * @param raw_sector the Sector to scramble
+     * @returns A Scrambled Sector containing the scrambled data
+     */
     ScrambledSector scramble(Mode2Sector raw_sector);
 
+    /**
+     * @brief Unscrambles the given Scrambled Sector
+     * @param scrambled_sector The Sector to unscramble
+     * @returns A Mode 2 Sector containing the unscrambled data
+     */
     Mode2Sector unscramble(ScrambledSector scrambled_sector);
 }
 
