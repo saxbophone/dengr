4221503542ab45b71fe906cca5378515e182cdf0
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-06 10:02:29 +0100

    Add copyright notices to python files

--- tests/build_efm_test_data_from_efm_table.py
+++ tests/build_efm_test_data_from_efm_table.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
-
 '''
+This source file forms part of DENGR, a piece of software which
+produces disc images which produce visible images on the recording side when
+burned to Compact Disc.
+
 This helper script can be used to re-generated the data-driven test data used
 to test the EFM encoder/decoder, the tests for which are in the file efm.cpp
+
+Copyright (C) 2020 Joshua Saxby <joshua.a.saxby@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 
--- tests/build_scrambling_test_data.py
+++ tests/build_scrambling_test_data.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
-from random import randint
+'''
+This source file forms part of DENGR, a piece of software which
+produces disc images which produce visible images on the recording side when
+burned to Compact Disc.
 
-
-"""
 This script generates N pairs of files containing data used to test the annex B
 scrambler --one input file consisting of raw sector data, and a corresponding
 output file containing what the data should be once it has been scrambled.
-"""
+
+Copyright (C) 2020 Joshua Saxby <joshua.a.saxby@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from random import randint
 
 
 # number of random sample data files to generate
