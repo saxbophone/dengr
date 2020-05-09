c2069bd163e5c971b36ac33f5a41130ebefbc73e
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-09 13:31:53 +0100

    Rewrote tests to include the previous pit value in test data

--- tests/physical_layer.cpp
+++ tests/physical_layer.cpp
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <utility>
+#include <tuple>
 
 #include <cstddef>
 
@@ -37,17 +37,19 @@ SCENARIO("Sequences of bits can be converted to sequences of pits/lands") {
         const std::size_t LENGTH = 8;
         // bits are stuffed into uints here for compactness
         auto bits_pits_combination = GENERATE(
-            std::pair<std::uint8_t, std::uint8_t>{0b01101001, 0b01001110}
+            // previous-pit pits          expected-bits
+            std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b01101001, 0b01001110)
         );
         // extract the bit patterns for use in the test case
         ChannelBitArray<LENGTH> bits;
         PitArray<LENGTH> pits;
         for (std::uint8_t i = 0; i < 8; i++) {
-            bits[i] = (bits_pits_combination.first & (1 << (7 - i))) != 0;
-            pits[i] = (Pit)((bits_pits_combination.second & (1 << (7 - i))) != 0);
+            bits[i] = (std::get<1>(bits_pits_combination) & (1 << (7 - i))) != 0;
+            pits[i] = (Pit)((std::get<2>(bits_pits_combination) & (1 << (7 - i))) != 0);
         }
         WHEN("The bits are passed into the pit/land encoder") {
-            PitArray<LENGTH> output = bits_to_pits(Pit::LAND, bits);
+            // also extract the previous pit value
+            PitArray<LENGTH> output = bits_to_pits(std::get<0>(bits_pits_combination), bits);
             THEN("The encoder should return a corresponding sequence of pits/lands") {
                 // output should be equal to pits, which is what's expected
                 REQUIRE(output == pits);
