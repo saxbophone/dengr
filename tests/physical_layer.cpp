e3a221ac93c98e395f9fbfb4d212fa838f5a4a46
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-09 16:29:41 +0100

    Shorten test data lines via typedef

--- tests/physical_layer.cpp
+++ tests/physical_layer.cpp
@@ -34,21 +34,23 @@ using namespace com::saxbophone::dengr::physical_layer;
 
 SCENARIO("Sequences of bits can be converted to/from sequences of pits/lands") {
     const std::size_t LENGTH = 8;
+    // convenience typedef to keep the test case data lines within limits
+    typedef std::tuple<Pit, std::uint8_t, std::uint8_t> TestData;
     // bits are stuffed into uints here for compactness
     auto bits_pits_combination = GENERATE(
-        //                                        previous-pit bits        pits
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b01101001, 0b01001110),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b01101001, 0b10110001),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b11101010, 0b10110011),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b00110011, 0b11011101),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b00100100, 0b11000111),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b00010000, 0b00011111),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b11001101, 0b10001001),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b11110111, 0b01011010),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b10010010, 0b11100011),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b00010010, 0b11100011),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::PIT , 0b10000000, 0b00000000),
-        std::tuple<Pit, std::uint8_t, std::uint8_t>(Pit::LAND, 0b00000000, 0b00000000)
+        //    previous-pit  bits        pits
+        TestData(Pit::LAND, 0b01101001, 0b01001110),
+        TestData(Pit::PIT , 0b01101001, 0b10110001),
+        TestData(Pit::LAND, 0b11101010, 0b10110011),
+        TestData(Pit::PIT , 0b00110011, 0b11011101),
+        TestData(Pit::PIT , 0b00100100, 0b11000111),
+        TestData(Pit::LAND, 0b00010000, 0b00011111),
+        TestData(Pit::LAND, 0b11001101, 0b10001001),
+        TestData(Pit::PIT , 0b11110111, 0b01011010),
+        TestData(Pit::LAND, 0b10010010, 0b11100011),
+        TestData(Pit::PIT , 0b00010010, 0b11100011),
+        TestData(Pit::PIT , 0b10000000, 0b00000000),
+        TestData(Pit::LAND, 0b00000000, 0b00000000)
     );
     // extract the bit patterns for use in the test case
     ChannelBitArray<LENGTH> bits;
